565213fcfdc60bf5854306f366fa8b9019756992
Kyle <kylefredin@gmail.com>
2021-03-02 19:46:34 -0700

    add some random tests

---
+++ src/index.js
@@ -0,0 +1,60 @@
+const { HasFirstNameCriteria } = require("./criteria/HasFirstNameCriteria");
+const { HasLastNameCriteria } = require("./criteria/HasLastNameCriteria");
+const { AndCriteria } = require("./criteria/AndCriteria");
+const { OrCriteria } = require("./criteria/OrCriteria");
+const { IsEvenCriteria } = require("./criteria/IsEvenCriteria");
+const { IsOddCriteria } = require("./criteria/IsOddCriteria");
+const { User } = require("./User");
+
+// create a user that has both first name and last name
+const userWithBoth = new User({ firstName: "Test", lastName: "User" });
+
+// create a user with only first name
+const userOnlyFirst = new User({ firstName: "Test" });
+
+// create a user with only last name
+const userOnlyLast = new User({ lastName: "User" });
+
+// create a user with no name
+const userWithNeither = new User();
+
+// create an array of all users
+const allUsers = [userWithBoth, userOnlyFirst, userOnlyLast, userWithNeither];
+
+console.log({ allUsers });
+
+// determine which users in the array have a first name
+const firstNameCriteria = new HasFirstNameCriteria();
+const usersWithFirstName = firstNameCriteria.meetCriteria(allUsers);
+
+console.log({ usersWithFirstName });
+
+// determine which users in the array have a last name
+const lastNameCriteria = new HasLastNameCriteria();
+const usersWithLastName = lastNameCriteria.meetCriteria(allUsers);
+
+console.log({ usersWithLastName });
+
+// determine which users in the array have first AND last name
+const bothCriteria = new AndCriteria([firstNameCriteria, lastNameCriteria]);
+const usersWithBoth = bothCriteria.meetCriteria(allUsers);
+
+console.log({ usersWithBoth });
+
+// determine which users in the array have first OR last name
+const eitherCriteria = new OrCriteria([firstNameCriteria, lastNameCriteria]);
+const usersWithEither = eitherCriteria.meetCriteria(allUsers);
+
+console.log({ usersWithEither });
+
+const numbers = [1, 1, 2, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+const oddCriteria = new IsOddCriteria();
+const oddNumbers = oddCriteria.meetCriteria(numbers);
+
+console.log({ oddNumbers });
+
+const evenCriteria = new IsEvenCriteria();
+const evenNumbers = evenCriteria.meetCriteria(numbers);
+
+console.log({ evenNumbers });
